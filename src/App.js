8f992ac63f020f01615b6d3cdd2def3f2a38c224
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-03-03 14:54:23 +0200

    Add Header Component

--- src/App.js
+++ src/App.js
@@ -1,6 +1,7 @@
 import './App.css';
 import React, { useEffect, useState } from 'react';
 import * as BooksAPI from './BooksAPI'
+import { Book } from './components';
 
 function App() {
   
@@ -11,10 +12,10 @@ useEffect(()=>{
 },[])
   console.log(books)
 
-  
+
   return (
     <div className="app">
- 
+ <Book title="tit" authors="au" />
       </div>
   );
 
