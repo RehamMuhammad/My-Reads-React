ee68f8d6eb6f4179ea039ff04e54e9dc801bca32
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-03-03 21:45:10 +0200

    Add Search Books Functionality

--- src/components/SearchInput/index.js
+++ src/components/SearchInput/index.js
@@ -2,14 +2,15 @@ import React from "react";
 import { Link } from 'react-router-dom'
 
 
-function SearchInput() {
+function SearchInput({searchWord, onSearch}) {
   return (
     <div className="search-books-bar">
       <Link to="/">
         <button className="close-search">Close</button>
       </Link>
       <div className="search-books-input-wrapper">
-        <input type="text" placeholder="Search by title or author" />
+        <input type="text" placeholder="Search by title or author" value={searchWord} onChange={onSearch}/>
+
       </div>
     </div>
   );
--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -1,10 +1,31 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { SearchInput } from '../../components'
+import * as BooksAPI from "../../BooksAPI"
+import { BookShelf } from '../../components';
 
 function SearchPage() {
+    const[searchWord, setSearchWord] = useState("");
+    const[resultBooks, setResultBooks] = useState([]);
+
+
+    const searchBook = async (query) =>{
+        setSearchWord(query)
+        await BooksAPI.search(searchWord).then((res) => setResultBooks(res))
+    } 
+
+    
+const updateShelf = async (book, shelfName) => {
+    await BooksAPI.update(book, shelfName);
+    console.log("Book",book,"Shelf",shelfName)
+  }
+
+    console.log(resultBooks)
     return (
         <div className="search-books">
-         <SearchInput />
+         <SearchInput searchWord={searchWord} onSearch={(e) => searchBook(e.target.value) } />
+         <BookShelf shelf="Search Results" books={resultBooks} updateShelf={updateShelf}/>
+
+
         </div>
     )
 }
