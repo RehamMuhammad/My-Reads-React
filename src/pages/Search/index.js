d7472f8019cd9c96a4b36ec815f629d7bdb7b697
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-03-03 23:42:46 +0200

    Reformat, Organize Search page & write comments

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -8,48 +8,32 @@ function SearchPage() {
     const [resultBooks, setResultBooks] = useState([]);
     const [books, setBooks] = useState([])
 
-
-
-    const searchBook = async (query) => {
-        setSearchWord(query)
-        await BooksAPI.search(query).then((res) => setResultBooks([...res, ...books])).catch((e)=>console.log(e))
-    }
-
-
-    const updateShelf = async (book, shelfName) => {
-        await BooksAPI.update(book, shelfName).catch((e)=>console.log(e))
-        console.log("Book", book, "Shelf", shelfName)
-    }
-
-
+    //Get All Books have the Search keyword on Initialization by getting all books Fun.
     useEffect(() => {
-        if (searchWord) {
-            getAllBooks()
-
-        } else {
-            setBooks([])
-            setResultBooks([])
-
-        }
-
+        getAllBooks()
     }, [searchWord])
 
     const getAllBooks = async () => {
-            await BooksAPI.getAll().then(res => setBooks(res.filter((book) => book.title === searchWord))).catch((e)=>console.log(e))
-
-        
-
+        await BooksAPI.getAll().then(res => setBooks(res.filter((book) => book.title === searchWord))).catch((e) => console.log(e))
     }
     console.log(books)
 
+    //Get All Books have the Search keyword by search Fun.
+    const searchBook = async (query) => {
+        setSearchWord(query)
+        await BooksAPI.search(query).then((res) => setResultBooks([...res, ...books])).catch((e) => console.log(e))
+    }
 
+    //Update Books Shelves
+    const updateShelf = async (book, shelfName) => {
+        await BooksAPI.update(book, shelfName).catch((e) => console.log(e))
+        console.log("Book", book, "Shelf", shelfName)
+    }
     console.log(resultBooks)
     return (
         <div className="search-books">
             <SearchInput searchWord={searchWord} onSearch={(e) => searchBook(e.target.value)} />
-            {searchWord && <BookShelf shelf="Search Results" books={resultBooks} updateShelf={updateShelf} />
-            }
-
+            {searchWord && <BookShelf shelf="Search Results" books={resultBooks} updateShelf={updateShelf} />}
         </div>
     )
 }
