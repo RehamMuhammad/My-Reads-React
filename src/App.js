f855d2a8b6267ee32f8f7ba35198b16956ce5cd4
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-03-03 14:38:13 +0200

    Get all books from books API

--- src/App.js
+++ src/App.js
@@ -1,11 +1,22 @@
-import logo from './logo.svg';
 import './App.css';
+import React, { useEffect, useState } from 'react';
+import * as BooksAPI from './BooksAPI'
 
 function App() {
-  return (
-    <div className="App">
-    </div>
-  );
-}
+  
+const [books, setBooks] = useState([])
 
+useEffect(()=>{
+  BooksAPI.getAll().then(res => setBooks(res) )
+},[])
+  console.log(books)
+
+  
+  return (
+    <div className="app">
+ 
+      </div>
+  );
+
+  }
 export default App;
