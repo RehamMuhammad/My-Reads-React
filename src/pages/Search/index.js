8064a4f000c1ed067125095bca3dc14dfa5bde1d
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-03-04 11:20:16 +0200

    Handle empty query error

--- src/pages/Search/index.js
+++ src/pages/Search/index.js
@@ -7,6 +7,7 @@ function SearchPage() {
     const [searchWord, setSearchWord] = useState("");
     const [resultBooks, setResultBooks] = useState([]);
     const [books, setBooks] = useState([])
+    const [error, setError] = useState("")
 
     //Get All Books have the Search keyword
     const searchBook = async (query) => {
@@ -29,7 +30,11 @@ function SearchPage() {
                 //Get All Books from searching books func. By title & Author
                 BooksAPI.search(query).then((res) => {
                     let searchResultBooks = res;
-                    setResultBooks(searchResultBooks)
+                    if (searchResultBooks.error){
+                        setError("No Reults Found")
+                    }else{
+                        setResultBooks(searchResultBooks)
+                    }
                 }).catch((e) => console.log(e))])
         }, 1000),
         []
@@ -44,10 +49,17 @@ function SearchPage() {
     console.log(resultBooks)
     console.log(_.uniqBy(books.concat(resultBooks), "id"))
 
+    console.log(error)
     return (
         <div className="search-books">
             <SearchInput searchWord={searchWord} onSearch={(e) => searchBook(e.target.value)} />
             {searchWord && (books.length > 0 || resultBooks.length > 0) && <BookShelf shelf="Search Results" books={_.uniqBy(books.concat(resultBooks), "id")} updateShelf={updateShelf} />}
+            {searchWord && error && books.length<1 && (
+                <div className='container my-5 py-5 text-center'>
+                    <p className="text-danger alert-danger py-5 my-5 fw-bold fs-1">{error}</p>
+                    </div>
+            )
+            }
         </div>
     )
 }
