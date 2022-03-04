113702b95268e49d2a06b9a979bfbdd3e1659e62
RehamMuhammad <reham.muhammad.helmy@gmail.com>
2022-03-04 13:26:38 +0200

    Get Single Book By id on routing to book details page

--- src/pages/BookDetails/index.js
+++ src/pages/BookDetails/index.js
@@ -1,9 +1,24 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
 import { BookCard } from '../../components'
+import * as BooksAPI from "../../BooksAPI"
+import { useParams } from 'react-router-dom'
 
 function BookDetails() {
+    let {id} = useParams()
+    const [book, setBook] = useState(null)
+
+    useEffect(() => {
+        getBookDetails(id)
+    },[])
+
+    const getBookDetails = async (bookId) => {
+        await BooksAPI.get(bookId).then((res) => setBook(res)).catch(e => console.log(e))
+    }
+
+    console.log(book)
     return (
         <div className='container py-5 my-5'>
+            {id}
             <BookCard />
         </div>
     )
